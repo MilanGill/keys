e2896a36317c9be51714afb156e9a4fb4426f911
Milan_Gill <milangill@yandex.ru>
2023-01-23 23:32:03 +0300

    First bean in spring core with using spring-context

---
+++ src/main/java/com/likesoft24/TestBean.java
@@ -0,0 +1,18 @@
+package com.likesoft24;
+
+public class TestBean {
+    private String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/main/java/com/likesoft24/TestSpring.java
@@ -0,0 +1,16 @@
+package com.likesoft24;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        TestBean testBean =  context.getBean("testBean", TestBean.class);
+        System.out.println(testBean.getName());
+
+        context.close();
+    }
+}
