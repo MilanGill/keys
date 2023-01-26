5988af997ec7690e010960c27f83018ab96c4ba0
Milan_Gill <milangill@yandex.ru>
2023-01-26 19:18:55 +0300

    Dependency Injection part 1

--- src/main/java/com/likesoft24/TestSpring.java
+++ src/main/java/com/likesoft24/TestSpring.java
@@ -7,10 +7,11 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
+        //Music music =  context.getBean("musicBean", Music.class);
 
-        Music music =  context.getBean("musicBean", Music.class);
+        //MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
         musicPlayer.PlayMusic();
 
